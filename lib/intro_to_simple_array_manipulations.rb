c0dc8e3543ee1a4be132879eb5206b2a32657d02
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-24 20:23:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
 next_country = "Niger"
-countries_in_western_africa.push
+countries_in_western_africa.push(next_country)
