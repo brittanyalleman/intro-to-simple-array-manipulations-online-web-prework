be0c6a56778e4d27a6645bf229527dfcac60a223
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-24 22:19:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, element)
   array.delete(element)
 end
 
-def using_delete_at(array, integer)
-  array.delete(integer)
+def using_delete_at(array, element)
+  array.delete(element)
 end
