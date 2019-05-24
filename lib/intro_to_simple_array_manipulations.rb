0c7d47cccec8b5d5a7689e0094795565a76eae89
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-24 21:51:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,6 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_esoteric_programming_languages, Malbolge)
-list_of_esoteric_programming_languages.insert(Malbolge)
+def using_insert(list_of_esoteric_programming_languages, malbolge)
+list_of_esoteric_programming_languages.insert(malbolge)
 end
