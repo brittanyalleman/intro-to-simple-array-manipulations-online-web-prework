bdf99e7c16609a00b7641b2ebaa2ef5a177af34c
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-24 20:53:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
-def using_push(countries_in_western_africa, next_country, Niger)
-countries_in_western_africa.push
+def using_push(countries_in_western_africa, next_country)
+countries_in_western_africa.push(next_country)
 end
